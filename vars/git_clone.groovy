77ed7ece7a2cd8a09eb5cb6372522d3ba54ba58a
Madhan Reddy <139326566+Madhan152004@users.noreply.github.com>
2026-04-05 18:47:11 +0530

    Updating git_clone

--- vars/git_clone.groovy
+++ vars/git_clone.groovy
@@ -1,3 +1,20 @@
-def call(String url, String branch, String credId){
-     git url:"${url}", branch:"${branch}", credentialsId:"${credId}"
+def call(Map config = [:]) {
+
+    def repoUrl = config.url
+    def branch = config.branch ?: 'main'
+    def credentialsId = config.credId ?: 'git-cred'
+    def retries = config.retries ?: 3
+
+    if (!repoUrl) {
+        error "Git URL is required"
+    }
+    retry(retries) {
+        git(
+            url: repoUrl,
+            branch: branch,
+            credentialsId: credentialsId
+        )
+    }
+
+    echo "✅ Git checkout successful"
 }
